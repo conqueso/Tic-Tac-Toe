ecc92484efbf9f90d6798a71c2d1a08a233e2413
Joe Jalbert <joseph.a.jalbert@gmail.com>
2013-12-01 16:25:41 -0500

    added possibility of a tie
    also some slight refactoring

--- TTT.rb
+++ TTT.rb
@@ -1,4 +1,3 @@
-
 board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 
 
@@ -23,71 +22,83 @@ end
 puts
 puts
 show(board)
+puts
+puts
 
 move_count = 1
+p = 'X'
 
-until ((board[0][0] == board[0][1] && board[0][1] == board[0][2]) && board[0][2] != ' ') or
+loop do
+  if  ((board[0][0] == board[0][1] && board[0][1] == board[0][2]) && board[0][2] != ' ') or
       ((board[1][0] == board[1][1] && board[1][1] == board[1][2]) && board[1][2] != ' ') or
       ((board[2][0] == board[2][1] && board[2][1] == board[2][2]) && board[2][2] != ' ') or
       ((board[0][0] == board[1][0] && board[1][0] == board[2][0]) && board[2][0] != ' ') or
       ((board[0][1] == board[1][1] && board[1][1] == board[2][1]) && board[2][1] != ' ') or
       ((board[0][2] == board[1][2] && board[1][2] == board[2][2]) && board[2][2] != ' ') or
       ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) && board[2][2] != ' ') or
-      ((board[2][0] == board[1][1] && board[1][1] == board[0][2]) && board[0][2] != ' ') do  
-    if move_count.odd?
-      p = 'X'
-    else
-      p = 'O'
-    end
-
-   loop do
-      puts
-      puts "Player #{p} enter a column number from 1-3:"
-      $c = gets.chomp.to_i
-      if $c > 0 && $c < 4
-        break
-      end
-      puts
-      puts
-      puts "****please enter a valid column number****"
-      puts
-    end
-
-    loop do
-      puts
-      puts "Player #{p} enter a row number from 1-3:"
-      $r = gets.chomp.to_i
-      if $r > 0 && $r < 4
-        break
-      end
-      puts "****please enter a valid row number**** "
-      puts
-    end
-
-    location = board[($r-1)][$c-1]
-
-    if location != ' '
-      puts
-      puts
-      puts "you can't go there!!!!!!!!"
-    else
-      board[($r-1)][$c-1] = p
-      move_count += 1  
-    end
-    
+      ((board[2][0] == board[1][1] && board[1][1] == board[0][2]) && board[0][2] != ' ')
+    puts
+    puts "#{p}s win!!!!!!!!"
+    puts
     show(board)
+    break
+  elsif board[0][0] != ' ' && board[0][1] != ' ' && board[0][2] != ' ' &&
+        board[1][0] != ' ' && board[1][1] != ' ' && board[1][2] != ' ' &&
+        board[2][0] != ' ' && board[2][1] != ' ' && board[2][2] != ' ' 
+    puts
+    puts "the game has ended in a draw"
+    puts
+    show(board)
+    break
+  end
+      
+  if move_count.odd?
+    p = 'X'
+  else
+    p = 'O'
+  end
 
- end
+ loop do
+    puts
+    puts "Player #{p} enter a column number from 1-3:"
+    $c = gets.chomp.to_i
+    if $c > 0 && $c < 4
+      break
+    end
+    puts
+    puts
+    puts "****please enter a valid column number****"
+    puts
+    puts
+  end
 
-if move_count.even?
-  p = 'X'
-else
-  p = 'O'
+  loop do
+    puts
+    puts "Player #{p} enter a row number from 1-3:"
+    $r = gets.chomp.to_i
+    if $r > 0 && $r < 4
+      break
+    end
+    puts
+    puts
+    puts "****please enter a valid row number**** "
+    puts
+    puts
+  end
+
+  location = board[($r-1)][$c-1]
+
+  if location != ' '
+    puts
+    puts
+    puts "you can't go there!!!!!!!!"
+    puts
+    puts
+  else
+    board[($r-1)][$c-1] = p
+    move_count += 1  
+  end
+  
+  show(board)
+
 end
-
-puts
-puts
-puts "#{p}s win!!!!!!!!"
-puts
-
-show(board)
